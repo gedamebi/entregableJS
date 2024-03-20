9ee2d149142ec850607969edb368e9c6d2bfc8b1
German Medina <urugerman@gmail.com>
2024-03-19 21:11:36 -0300

    Agrego archivo Readem

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,12 @@ let alumnosJson
 let alumnosLS = localStorage.getItem("alumnos")
 
 async function cargarAlumnosDB(){
+    /* 
+        Cargo el contenido del json en una array donde lo muestro fijo en las primeras filas de la tabla del DOM
+        Este contenido es fijo, es decir no podemos alterarlo desde el front por eso se quitan los elementos de eliminacion y edicion
+        No se podran ordenar estos elementos en la tabla mostrandolo siempre al principio en el orden que esta en el Json
+        Estos datos quedan diferenciados en la tabla con un background-color diferente el resto de las filas
+    */
     await fetch("../../../db/dataAlumnos.json")
         .then(response => response.json())
         .then(data => {
@@ -22,8 +28,14 @@ async function cargarAlumnosDB(){
             });
         });
 
+    /* 
+        Cargo el array de alumnos cargados en el storage
+        Estos si podran ser modificados por medio de la columna Accion de la tabla y tambien se podra agregar nuevos alumnos
+    */
     if (alumnosLS){
         alumnos = JSON.parse(alumnosLS)
+    } else {
+        alumnos = []
     }
 }
 
@@ -43,6 +55,12 @@ let cursosJson
 let cursosLS = localStorage.getItem("cursos")
 
 async function cargarCursosDB(){
+    /* 
+        Cargo el contenido del json en una array donde lo muestro fijo en las primeras filas de la tabla del DOM
+        Este contenido es fijo, es decir no podemos alterarlo desde el front por eso se quitan los elementos de eliminacion y edicion
+        No se podran ordenar estos elementos en la tabla mostrandolo siempre al principio en el orden que esta en el Json
+        Estos datos quedan diferenciados en la tabla con un background-color diferente el resto de las filas
+    */
     await fetch("../../../db/dataCursos.json")
         .then(response => response.json())
         .then(data => {
@@ -60,8 +78,14 @@ async function cargarCursosDB(){
             });
         });
 
+    /* 
+        Cargo el array de alumnos cargados en el storage
+        Estos si podran ser modificados por medio de la columna Accion de la tabla y tambien se podra agregar nuevos alumnos
+    */
     if (cursosLS){
         cursos = JSON.parse(cursosLS)
+    } else {
+        cursos = []
     }
 }
 
